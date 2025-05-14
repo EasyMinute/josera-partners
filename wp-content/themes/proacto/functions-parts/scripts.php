fde3f81d6958d1acbd638d98c96e1178fbe1564b
root <root@ubuntu-18-wordpress-josera>
2025-05-14 16:42:56 +0200

    Updates for main js file

--- wp-content/themes/proacto/functions-parts/scripts.php
+++ wp-content/themes/proacto/functions-parts/scripts.php
@@ -18,9 +18,9 @@
 
 		wp_enqueue_style( 'proacto-awesome-style', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css', array(), _S_VERSION );
 
-		wp_enqueue_style( 'proacto-main-style', get_template_directory_uri() . '/css/main.css', array(), _S_VERSION );
+		wp_enqueue_style( 'proacto-main-style', get_template_directory_uri() . '/css/main.css', array(), '1.0.1' );
 
-		wp_enqueue_style( 'proacto-footer-style', get_template_directory_uri() . '/css/footer.css', array(), _S_VERSION );
+		wp_enqueue_style( 'proacto-footer-style', get_template_directory_uri() . '/css/footer.css', array(),'1.0.1' );
 
 
 		wp_enqueue_script( 'select2-js', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', array( 'jquery' ), null, true );
@@ -31,9 +31,9 @@
 
 		wp_enqueue_script( 'proacto-google-maps-cluster', 'https://unpkg.com/@googlemaps/markerclustererplus/dist/index.min.js', array(), _S_VERSION, true );
 
-		wp_enqueue_script( 'proacto-main', get_template_directory_uri() . '/js/main.js', array(), 1.0, true );
+		wp_enqueue_script( 'proacto-main', get_template_directory_uri() . '/js/main.js', array(), _S_VERSION, true );
 
-		wp_enqueue_script( 'proacto-map', get_template_directory_uri() . '/js/map.js', array(), 1.0, true );
+		wp_enqueue_script( 'proacto-map', get_template_directory_uri() . '/js/map.js', array(), _S_VERSION, true );
 
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
